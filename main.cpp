c338c7eb8558223a36ec501d63dca61d68cd7ebe
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-08 19:25:40 +0200

    close the window if the user click on close window button

--- main.cpp
+++ main.cpp
@@ -10,24 +10,25 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-freopen("CON", "w", stdout);
-freopen("CON", "r", stdin);
-freopen("CON", "w", stderr);
-    Jouer jeu;
-    Window g(12*34,12*34);  //
-    g.SetColor(255,255,255);
-    g.HeaderTitle("Mario Sokoban by Gomez Guillaume");
-// Activation de la répétition des touches
-SDL_Event ev_menu;
-        Image im_menu("menu.jpg");
-        bool continuer=true;
+  freopen("CON", "w", stdout);
+  freopen("CON", "r", stdin);
+  freopen("CON", "w", stderr);
+  Jouer jeu;
+  Window g(12*34,12*34);  //
+  g.SetColor(255,255,255);
+  g.HeaderTitle("Mario Sokoban by Gomez Guillaume");
+  // Activation de la répétition des touches
+  SDL_Event ev_menu;
+  Image im_menu("menu.jpg");
+  bool continuer=true;
 
  while (continuer)
-{
+ {
     SDL_WaitEvent(&ev_menu);
     switch(ev_menu.type)
     {
         case SDL_QUIT:
+            g.Quit();
             continuer = false;
             break;
         case SDL_KEYDOWN: /* Si appui d'une touche */
@@ -35,33 +36,28 @@ SDL_Event ev_menu;
             break;
     }
     im_menu.BlitSurfaces(g,im_menu);
-g.Update();
+    g.Update();
 }
 
 
-SDL_ShowCursor(SDL_DISABLE);
-SDL_EnableKeyRepeat(100, 100);
+  SDL_ShowCursor(SDL_DISABLE);
+  SDL_EnableKeyRepeat(100, 100);
   while( g.runningState()) //while the user don't closed the window
-   {
+  {
     jeu.ReadFile();
-
     jeu.ChangeIndice();
-
-
-         do
-           {
-            jeu.Input(g);
-            g.FillRect();
-            jeu.BlitterEcran(g);
-            //Update
-            g.Update();
-           }  while( g.runningState()==true && jeu.TestWin()==false);
-           jeu.ChangerNiveau(g);
-            jeu.ViderSurface();  //permet d' "effacer" les surfaces qui contiendrait les caisses_ok
-   }
-
-    //destruction de l'instance window par le destructeur
-
-    return 0;
+    do
+      {
+        jeu.Input(g);
+        g.FillRect();
+        jeu.BlitterEcran(g);
+        //Update
+        g.Update();
+      }  while( g.runningState()==true && jeu.TestWin()==false);
+      jeu.ChangerNiveau(g);
+      jeu.ViderSurface();  //permet d' "effacer" les surfaces qui contiendrait les caisses_ok
+  }
+//destruction de l'instance window par le destructeur
+return 0;
 
 }
